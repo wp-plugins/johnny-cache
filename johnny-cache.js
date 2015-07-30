c29dfc7ca1770ce17c3527ca3ef7b1684343ded6
wonderboymusic <wonderboymusic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-30 19:32:46 +0000

    Updated code
    git-svn-id: https://plugins.svn.wordpress.org/johnny-cache/trunk@1209916 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- johnny-cache.js
+++ johnny-cache.js
@@ -1,98 +1,99 @@
-"use strict";
-
-/*globals window, document, $, jQuery */
+/*globals window, document, $, jQuery, ajaxurl */
 
 (function ($) {
-    var instance_store, refr, current, slctr;
+	"use strict";
+
+	var $instanceStore,
+		$refreshInstance,
+		$instanceSelector;
 
     function remove_group(el) {
         return function () {
-            el.closest('tr').fadeOut().remove();
+            $( el ).closest('tr').fadeOut().remove();
         };
     }
-    
+
     function remove_item(el) {
         return function () {
-            el.closest('p').fadeOut().remove();
+            $( el ).closest('p').fadeOut().remove();
         };
     }
 
-    function add_instance(data) {
-        instance_store.html(data);
-        refr.show();
-    }
+	function handleChange( e ) {
+		var $el = $( e.currentTarget ), val;
+
+		val = $.trim( $el.val() );
+
+		if ( val ) {
+			$.ajax({
+				type: 'post',
+				url : ajaxurl,
+				data : {
+					action : 'jc-get-instance',
+					nonce  : $el.data('nonce'),
+					name   : val
+				},
+				cache : false,
+				success : function ( data ) {
+					if ( data ) {
+						$instanceStore.html( data );
+						$refreshInstance.show();
+					}
+				}
+			});
+		}
+	}
 
     $(document).ready(function () {
-        instance_store = $('#instance-store');
-        refr = $('#refresh-instance');
-        slctr = $('#instance-selector');
-        
-        refr.click(function () {
-            slctr.trigger('change');
+        $instanceStore = $('#instance-store');
+        $refreshInstance = $('#refresh-instance');
+        $instanceSelector = $('#instance-selector');
+
+        $refreshInstance.click(function () {
+            $instanceSelector.trigger('change');
             return false;
         });
-        
-        slctr.bind('change', function () {
-            var val = $(this).val();
-            if ('' !== jQuery.trim(val)) {
-                var elem = $(this);
-                $.ajax({
-                    url     : '/wp-admin/admin-ajax.php',
-                    data    : {
-                        action : 'jc-get-instance',
-                        nonce  : elem.attr('data-nonce'),
-                        name   : val
-                    },
-                    cache   : false,
-                    success : function (data) {
-                        if (data) {
-                            add_instance(data);
-                            current = val;
-                        }
-                    }
-                });               
-            }
-        });
-        
-        $('.jc-flush-group').live('click', function () { 
-            var elem = $(this), keys = [];
-            
-            elem.parents('td').next().find('p').each(function () {
-                keys.push($(this).attr('data-key'));
-            });
-            
-            $.ajax({ 
-                url     : elem.attr('href'),
-                type    : 'post',
-                data    : {
-                    keys: keys
-                },
-                success : remove_group(elem)
-            });
-            return false; 
-        });
-        $('.jc-remove-item').live('click', function () { 
-            var elem = $(this);
-            $.ajax({
-                url     : elem.attr('href'),
-                success : remove_item(elem)
-            });
-            return false; 
-        });
-        
-        $('.jc-view-item').live('click', function () {
-            var elem = $(this);
-            $.ajax({ 
-                url     : elem.attr('href'),
-                type    : 'post',
-                success : function (data) {
-                    $('#debug').html(data);
-                    window.location.hash = 'jc-wrapper';
-                }
-            });
-            
-            return false;
-        });
+
+        $instanceSelector.bind( 'change', handleChange );
+
+        $( document.body )
+			.on('click', '.jc-flush-group', function (e) {
+				var elem = $( e.currentTarget ), keys = [];
+
+				elem.parents('td').next().find('p').each(function () {
+					keys.push($(this).data('key'));
+				});
+
+				$.ajax({
+					url : e.currentTarget.href,
+					type : 'post',
+					data : {
+						keys: keys
+					},
+					success : remove_group( elem[0] )
+				});
+				return false;
+			})
+			.on('click', '.jc-remove-item', function (e) {
+				$.ajax({
+					type : 'post',
+					url : e.currentTarget.href,
+					success : remove_item( e.currentTarget )
+				});
+				return false;
+			})
+			.on( 'click', '.jc-view-item', function (e) {
+				$.ajax({
+					url : e.currentTarget.href,
+					type : 'post',
+					success : function (data) {
+						$('#debug').html(data);
+						window.location.hash = 'jc-wrapper';
+					}
+				});
+
+				return false;
+			} );
     });
 
 }(jQuery));
